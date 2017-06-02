28debdfcd78940b50ef472b37a8a0c7fb870de0f
Antoine Gervail <antoine@sogilis.com>
2017-06-02 17:35:46 +0200

    Added feature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 from pony.orm import *
 from datetime import datetime, timedelta
 import os
+import json
 
 app = Flask(__name__)
 db = Database()
@@ -11,6 +12,12 @@ class SensorData(db.Entity):
     name = Required(str)
     dataType = Required(str)
     data = Required(float)
+
+class OrderData(db.Entity):
+    date = Required(datetime)
+    nodeId = Required(int)
+    orderType = Required(str)
+    orderData = Required(int)
 
 db.bind('postgres', user=os.environ["DB_USERNAME"], password=os.environ["DB_PASSWORD"], host=os.environ["DB_HOST"], database=os.environ["DB_NAME"], port=os.environ["DB_PORT"])
 db.generate_mapping(create_tables=True)
@@ -25,19 +32,6 @@ def sensor_list():
             types_list[n] = k
         print types_list
     return render_template('sensor_list.html', sensors=names, types_list=types_list)
-
-'''
-@app.route("/sensor_list")
-def sensor_list():
-    with db_session:
-        types_list = {}
-        names = select((s.name) for s in SensorData)[:]
-        for n in names:
-            k = select(d.dataType for d in SensorData if d.name == n)[:]
-            types_list[n] = k
-        print types_list
-    return render_template('sensor_list.html', sensors=names, types_list=types_list)
-'''
 
 @app.route('/post_sensor_data', methods=['GET'])
 def post_sensor_data():
@@ -61,12 +55,66 @@ def get_sensor_data():
             dates = []
             data = []
             for line in db_result:
-                dates.append(line.date)
+                dates.append(line.date.isoformat())
+                #print 'one : ' + str(line.date)
+                #print 'two : ' + str(line.date.isoformat())
                 data.append(line.data)
                 print line.date, line.data
         return render_template('graph.html', dates=dates, data=data, data_type=received_args['type'])
     else:
         return redirect(url_for('sensor_list'))
+
+@app.route('/get_orders', methods=['GET'])
+def get_orders():
+    received_args = request.args
+    with db_session:
+        if 'nodeId' in received_args:
+            next_order = select(d for d in OrderData if (d.date >= datetime.now()
+                                                         and d.nodeId == received_args['nodeId'])).order_by(OrderData.date).first()
+            if next_order:
+                res = {}
+                res['date'] = int((next_order.date - datetime.now()).total_seconds())
+                res['nodeId'] = next_order.nodeId
+                res['orderType'] = next_order.orderType
+                res['orderData'] = next_order.orderData
+
+                return json.dumps(res)
+            else:
+                return '{}'
+            #return str(next_order.date) + ' # ' + str(next_order.nodeId) + ' # ' + str(next_order.orderType) + '# ' + str(next_order.orderData) + '<br>'
+            #db_result = list(db_result)[0]
+        else:
+            db_result = select(d for d in OrderData if (d.date >= datetime.now() - timedelta(days=1))).order_by(OrderData.date)[:]
+            dates = []
+            data = []
+            res = ''
+            for line in db_result:
+                print line
+                res += str(line.date) + ' # ' + str(line.nodeId) + ' # ' + str(line.orderType) + '# ' + str(line.orderData) + '<br>'
+
+                #print 'one : ' + str(line.date)
+                #print 'two : ' + str(line.date.isoformat())
+                #data.append(line.data)
+                #print line.date, line.data
+        return res#render_template('graph.html', dates=dates, data=data, data_type=received_args['type'])
+
+@app.route('/post_order', methods=['GET'])
+def post_order():
+    received_args = request.args
+    if 'nodeId' in received_args and 'orderType' in received_args and 'orderDate' in received_args and 'orderData' in received_args:
+        print int(received_args['nodeId'])
+        print received_args['orderType']
+        #print int(received_args['orderDate'])
+        print received_args['orderData']
+        dateO = datetime.strptime(received_args['orderDate'],'%d/%m/%Y-%H:%M')
+        print dateO
+        with db_session:
+            od = OrderData(nodeId=int(received_args['nodeId']), orderType=received_args['orderType'],
+                               orderData=int(received_args['orderData']), date=dateO)
+            commit()
+        return "OK"
+    else:
+        return "BAD ARGS"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv("PORT"))
