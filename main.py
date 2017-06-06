15224ceac48fb989130d4c3efc906136a19d8624
Antoine Gervail <antoine@sogilis.com>
2017-06-06 15:04:09 +0200

    added orders

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ class SensorData(db.Entity):
     dataType = Required(str)
     data = Required(float)
 
-class OrderData(db.Entity):
+class OrdersData(db.Entity):
     date = Required(datetime)
     nodeId = Required(int)
     orderType = Required(str)
     orderData = Required(int)
+    orderStatus = Required(str)
 
 db.bind('postgres', user=os.environ["DB_USERNAME"], password=os.environ["DB_PASSWORD"], host=os.environ["DB_HOST"], database=os.environ["DB_NAME"], port=os.environ["DB_PORT"])
 db.generate_mapping(create_tables=True)
@@ -41,7 +42,7 @@ def post_sensor_data():
     print received_args['data']
     with db_session:
         sd = SensorData(name=received_args['name'], dataType=received_args['data_type'], data=float(received_args['data']), date=datetime.now())
-        commit()
+        #commit()
     return "OK"
 
 @app.route('/get_sensor_data', methods=['GET'])
@@ -69,20 +70,22 @@ def get_orders():
     received_args = request.args
     with db_session:
         if 'nodeId' in received_args:
-            next_order = select(d for d in OrderData if (d.date >= datetime.now()
-                                                         and d.nodeId == received_args['nodeId'])).order_by(OrderData.date).first()
+            next_order = select(d for d in OrdersData if (d.date >= datetime.now()
+                                                         and d.nodeId == received_args['nodeId']
+                                                         and d.orderStatus == 'PENDING')).order_by(OrdersData.date).first()
             if next_order:
                 res = {}
                 res['date'] = int((next_order.date - datetime.now()).total_seconds())
                 res['nodeId'] = next_order.nodeId
                 res['orderType'] = next_order.orderType
                 res['orderData'] = next_order.orderData
-
+                res['orderStatus'] = next_order.orderStatus
+                res['key'] = next_order.id
                 return json.dumps(res)
             else:
                 return '{}'
         else:
-            db_result = select(d for d in OrderData if (d.date >= datetime.now() - timedelta(days=1))).order_by(OrderData.date)[:]
+            db_result = select(d for d in OrdersData if (d.date >= datetime.now() - timedelta(days=1))).order_by(OrdersData.date)[:]
             res = []
             for line in db_result:
                 cur = {}
@@ -90,8 +93,22 @@ def get_orders():
                 cur['nodeId'] = line.nodeId
                 cur['orderType'] = line.orderType
                 cur['orderData'] = line.orderData
+                cur['orderStatus'] = line.orderStatus
+                cur['key'] = line.id
                 res.append(cur)
             return json.dumps(res)
+
+@app.route('/delete_order', methods=['GET'])
+def delete_order():
+    received_args = request.args
+    with db_session:
+        if 'orderId' in received_args:
+            next_order = select(o for o in OrdersData if o.id == int(received_args['orderId'])).first()
+            if next_order:
+                next_order.orderStatus = 'DONE'
+            return 'removed ' + received_args['orderId']
+    return 'wrong request format'
+
 
 @app.route('/post_order', methods=['GET'])
 def post_order():
@@ -103,9 +120,9 @@ def post_order():
         dateO = datetime.strptime(received_args['orderDate'],'%d/%m/%Y-%H:%M')
         print dateO
         with db_session:
-            od = OrderData(nodeId=int(received_args['nodeId']), orderType=received_args['orderType'],
-                               orderData=int(received_args['orderData']), date=dateO)
-            commit()
+            od = OrdersData(nodeId=int(received_args['nodeId']), orderType=received_args['orderType'],
+                               orderData=int(received_args['orderData']), date=dateO, orderStatus='PENDING')
+            #commit()
         return "OK"
     else:
         return "BAD ARGS"
